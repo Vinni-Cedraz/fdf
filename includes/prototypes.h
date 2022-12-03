3a91db89ad99068ba205c3c22d66c969a67f7a98
Vinni-Cedraz <planetexpress0101@gmail.com>
2022-12-03 15:46:28 -0300

    lines working

--- includes/prototypes.h
+++ includes/prototypes.h
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 11:52:12 by vcedraz-          #+#    #+#             */
-/*   Updated: 2022/12/03 14:04:17 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2022/12/03 15:44:34 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 
 void	put_pixel_img(t_img *img, int x, int y, int color);
 
-void	render_square(t_data *data, t_img *img, int color, t_square *square);
+void	render_square(t_img *img, int color, t_square *square);
 
-void	render_line(t_point point_a, t_point point_b, int color, t_img *img);
+void	render_line(t_point p1, t_point p2, int color, t_img *img);
 
 void	linedraw(t_img *img, t_point *point1, t_point *point2);
 
 int		deal_key(int key, t_data *data);
 
-void	open_window(t_data *data, t_img *img);
+void	open_win_n_img(t_data *data, t_img *img);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,17 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/03 11:51:05 by vcedraz-          #+#    #+#             */
+/*   main.c                                             :+:      :+:    :+:   */ /*                                                    +:+ +:+         +:+     */ /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */ /*                                                +#+#+#+#+#+   +#+           */ /*   Created: 2022/12/03 11:51:05 by vcedraz-          #+#    #+#             */
 /*   Updated: 2022/12/03 14:07:20 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes.h"
-#include "structs.h"
 
 int	main(void)
 {
@@ -28,9 +23,9 @@ int	main(void)
 	white_square.height_start = 0;
 	white_square.total_width = 500;
 	white_square.total_height = 500;
-	open_window(&data, &img);
-	render_square(&data, &img, WHITE, &white_square);
-	render_square(&data, &img, RED, &red_square);
+	open_win_n_img(&data, &img);
+	render_square(&img, WHITE, &white_square);
+	render_square(&img, RED, &red_square);
 	render_line((t_point){0, 0, 0}, (t_point){500, 500, 0}, RED, &img);
 	render_line((t_point){500, 0, 0}, (t_point){0, 500, 0}, RED, &img);
 	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, img.mlx_img, 0, 0);
