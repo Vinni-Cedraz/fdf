4ad603a1f1c2299ecf3a677bb99831fab2e042f7
Vinni-Cedraz <planetexpress0101@gmail.com>
2022-12-09 14:37:05 -0300

    t_split apparently has to be passed as a static to avoid leaks

--- srcs/render_map.c
+++ srcs/render_map.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 18:45:33 by vcedraz-          #+#    #+#             */
-/*   Updated: 2022/12/08 23:26:17 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2022/12/09 14:02:45 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ typedef struct s_map
 	int		j;
 	int		numbers;
 	int		lines;
-	int		map[900][900];
+	int		map[11][19];
 	char	*line;
 }			t_map;
 
@@ -30,21 +30,21 @@ void	render_map(int fd)
 	t_split	*split;
 	
 	map.lines = 0;
-	map.numbers = -1;
+	map.i = -1;
 	while(1)
 	{
 	  map.line = ft_gnl(fd);
 	  if (!map.line)
 	    break;
 	  split = ft_split(map.line, ' ');
+	  map.numbers = (int)split->words;
 	  free(map.line);
-	  while (++map.numbers < (int)split->words - 1)
-	    map.map[map.lines][map.numbers] = ft_atoi(split->str_arr[map.numbers]);
-	  map.numbers = -1;
+	  while (++map.i < (int)split->words - 1)
+	    map.map[map.lines][map.i] = ft_atoi(split->str_arr[map.i]);
+	  map.i = -1;
 	  map.lines++;
+	  ft_free_arr(split->str_arr, (void **)split->str_arr);
 	}
-	map.numbers = (int)split->words;
-	ft_free_arr(split->str_arr, (void **)split->str_arr);
 	printf_map(&map);
 }
 static void	printf_map(t_map *map)
