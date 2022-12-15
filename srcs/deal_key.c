ac1532e352db78438f5fcb77d7cbb94a26909d6d
Vinnícius Cedraz Ribeiro <vcedraz-@student42sp.org.br>
2022-12-15 16:43:24 -0300

    mandatory 100% done

--- srcs/deal_key.c
+++ srcs/deal_key.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 13:15:32 by vcedraz-          #+#    #+#             */
-/*   Updated: 2022/12/14 20:05:24 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2022/12/15 16:42:03 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,15 @@ int	deal_key(int key, t_data *data)
 		exit(0);
 	}
 	return (0);
+}
+
+int	close_win(t_data *data)
+{
+	mlx_destroy_image(data->mlx_ptr, data->img->mlx_img);
+	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	mlx_destroy_display(data->mlx_ptr);
+	free(data->mlx_ptr);
+	free(data->img);
+	ft_free_t_data(&data);
+	exit(0);
 }
