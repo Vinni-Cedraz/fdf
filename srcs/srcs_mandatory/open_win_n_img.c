44acb2799a756472faa3a6f3650288f54fd66ab8
Vinni-Cedraz <planetexpress0101@gmail.com>
2022-12-16 12:16:40 -0300

    totally submitable and some bonuses done

--- srcs/srcs_mandatory/open_win_n_img.c
+++ srcs/srcs_mandatory/open_win_n_img.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 11:55:36 by vcedraz-          #+#    #+#             */
-/*   Updated: 2022/12/16 02:27:25 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2022/12/16 11:26:04 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ void	open_win_n_img(t_data *data)
 	data->mlx_ptr = mlx_init();
 	data->img->width = WINDOW_WIDTH;
 	data->img->height = WINDOW_HEIGHT;
-	data->win_ptr = mlx_new_window(data->mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,
-			"mlx 42");
+	data->win_ptr = mlx_new_window(data->mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, \
+	"mlx 42");
 	open_img(data);
 }
 
