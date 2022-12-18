df80b78847abded141ebdb416da9527f5d8821e1
Vinni-Cedraz <vcedraz-@student.42sp.org.br>
2022-12-17 22:23:22 -0300

    debug messages, found problem

--- srcs/srcs_bonus/apply_left_hand_rule.c
+++ srcs/srcs_bonus/apply_left_hand_rule.c
@@ -6,13 +6,13 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 15:04:33 by vcedraz-          #+#    #+#             */
-/*   Updated: 2022/12/17 15:11:42 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2022/12/17 21:10:41 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf_includes_bonus.h"
 
-static	void	aux_apply_left_hand_rule(t_point *point);
+static void	aux_apply_left_hand_rule(t_point *point);
 
 int	apply_left_hand_rule(t_data *d)
 {
@@ -20,14 +20,14 @@ int	apply_left_hand_rule(t_data *d)
 	aux_apply_left_hand_rule(*d->map->arr);
 	render_map_bns(d);
 	mlx_put_image_to_window(d->mlx_ptr, d->win_ptr, d->img, 0, 0);
-	return(0);
+	return (0);
 }
 
-static	void	aux_apply_left_hand_rule(t_point *point)
+static void	aux_apply_left_hand_rule(t_point *point)
 {
-	int		x;
-	int		y;
-	int		z;
+	int	x;
+	int	y;
+	int	z;
 
 	x = point->x;
 	y = point->y;
