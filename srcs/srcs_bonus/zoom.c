f7e0011d7458bc09aae446e07d63fa795b7ed2a2
Vinni-Cedraz <planetexpress0101@gmail.com>
2022-12-21 16:31:34 -0300

    norm ok, zoom centralized

--- srcs/srcs_bonus/zoom.c
+++ srcs/srcs_bonus/zoom.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 15:38:50 by vcedraz-          #+#    #+#             */
-/*   Updated: 2022/12/21 15:42:50 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2022/12/21 16:31:14 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,20 @@
 
 static void	aux_zoom_in(t_data *d);
 static void	aux_zoom_out(t_data *d);
+static void	find_center(t_data *d);
+
+static void	find_center(t_data *d)
+{
+	d->cx = (double)d->map->width / 2;
+	d->cy = (double)d->map->height / 2;
+	d->cz = (double)d->map->max_z / 2;
+}
 
 void	zoom_in(t_data *d)
 {
 	if (d->clockwise - d->counter_clock != 1)
 		return ;
+	find_center(d);
 	aux_zoom_in(d);
 }
 
@@ -40,8 +49,10 @@ static void	aux_zoom_in(t_data *d)
 		j = 0;
 		while (j < d->map->width)
 		{
-			d->map->arr[i][j].x = (d->map->arr[i][j].x - d->cx) * 1.1 + d->cx;
-			d->map->arr[i][j].y = (d->map->arr[i][j].y - d->cy) * 1.1 + d->cy;
+			d->map->arr[i][j].x = (d->map->arr[i][j].x - d->cx) * 1.1 + d-> \
+				cx * 0.9;
+			d->map->arr[i][j].y = (d->map->arr[i][j].y - d->cy) * 1.1 + d-> \
+				cy * 0.9;
 			j++;
 		}
 		i++;
@@ -59,8 +70,10 @@ static void	aux_zoom_out(t_data *d)
 		j = 0;
 		while (j < d->map->width)
 		{
-			d->map->arr[i][j].x = (d->map->arr[i][j].x - d->cx) * 0.9 + d->cx;
-			d->map->arr[i][j].y = (d->map->arr[i][j].y - d->cy) * 0.9 + d->cy;
+			d->map->arr[i][j].x = (d->map->arr[i][j].x - d->cx) * 0.9 + d-> \
+				cx * 1.1;
+			d->map->arr[i][j].y = (d->map->arr[i][j].y - d->cy) * 0.9 + d-> \
+				cy * 1.1;
 			j++;
 		}
 		i++;
