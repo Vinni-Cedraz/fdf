84040713d5324cc2395672b3c1e00a7c18c1d5a3
Vinni-Cedraz <planetexpress0101@gmail.com>
2022-12-29 23:06:12 -0300

    mandatory was decentralized because it's isometric steps got changed when I renamed the macros

--- srcs/srcs_mandatory/rotate_map.c
+++ srcs/srcs_mandatory/rotate_map.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 17:44:56 by vcedraz-          #+#    #+#             */
-/*   Updated: 2022/12/29 20:25:06 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2022/12/29 23:03:53 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,8 @@ void	rotate_map(t_map *map)
 		{
 			if (map->arr[i][j].z != 0)
 				map->arr[i][j].z *= map->scale_z;
-			rotate_z(&map->arr[i][j], RAD_54_73);
-			rotate_x(&map->arr[i][j], RAD_45);
+			rotate_z(&map->arr[i][j], RAD_45);
+			rotate_x(&map->arr[i][j], RAD_54_73);
 			j++;
 		}
 		i++;
