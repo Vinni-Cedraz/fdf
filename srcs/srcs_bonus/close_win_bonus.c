3c0c7f7381e6d2f041419c1a8a4ed29eff0198b9
Vinni-Cedraz <planetexpress0101@gmail.com>
2022-12-31 19:18:35 -0300

    refactoring microcommit 11 - t_mlx implemented suuccesfully

--- srcs/srcs_bonus/close_win_bonus.c
+++ srcs/srcs_bonus/close_win_bonus.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 03:24:36 by vcedraz-          #+#    #+#             */
-/*   Updated: 2022/12/31 16:41:25 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2022/12/31 19:15:09 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,11 @@ int	close_win_bonus(t_data *data)
 	else if (data->neutral_x == 0)
 		two_steps_to_isometry_bonus(data, 1);
 	free(data->rotations_history);
-	mlx_destroy_image(data->mlx_ptr, data->img->mlx_img);
-	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-	mlx_destroy_display(data->mlx_ptr);
-	free(data->mlx_ptr);
+	mlx_destroy_image(data->mlx->mlx_ptr, data->img->mlx_img);
+	mlx_destroy_window(data->mlx->mlx_ptr, data->mlx->win_ptr);
+	mlx_destroy_display(data->mlx->mlx_ptr);
+	free(data->mlx->mlx_ptr);
+	free(data->mlx);
 	free(data->img);
 	ft_free_t_map(data->map->arr);
 	free(data->map);
--- srcs/srcs_bonus/data_initializer_bonus.c
+++ srcs/srcs_bonus/data_initializer_bonus.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 14:06:45 by vcedraz-          #+#    #+#             */
-/*   Updated: 2022/12/31 16:41:14 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2022/12/31 19:15:23 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ static void	open_win_and_img(t_data *data);
 
 void	data_initializer_bonus(t_data *d)
 {
+	d->mlx = malloc(sizeof(t_mlx));
 	open_win_and_img(d);
 	d->grid_style_nb = 1;
 	d->rotations_history = ft_lstnew(NULL);
@@ -39,12 +40,12 @@ void	data_initializer_bonus(t_data *d)
 
 static void	open_win_and_img(t_data *data)
 {
-	data->mlx_ptr = mlx_init();
+	data->mlx->mlx_ptr = mlx_init();
 	data->img->width = WINDOW_WIDTH;
 	data->img->height = WINDOW_HEIGHT;
-	data->win_ptr = mlx_new_window(data->mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,
+	data->mlx->win_ptr = mlx_new_window(data->mlx->mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,
 			"mlx 42");
-	data->img->mlx_img = mlx_new_image(data->mlx_ptr, WINDOW_WIDTH,
+	data->img->mlx_img = mlx_new_image(data->mlx->mlx_ptr, WINDOW_WIDTH,
 			WINDOW_HEIGHT);
 	data->img->addr = mlx_get_data_addr(data->img->mlx_img, &data->img->bpp,
 			&data->img->line_len, &data->img->endian);
