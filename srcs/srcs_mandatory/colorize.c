f43381a9a3fd3d7ff73e7a387286760549332451
Vinni-Cedraz <vcedraz-@student.42sp.org.br>
2023-01-07 00:17:11 -0300

    more aesthetic pointer dereferencing syntax

--- srcs/srcs_mandatory/colorize.c
+++ srcs/srcs_mandatory/colorize.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 23:27:42 by vcedraz-          #+#    #+#             */
-/*   Updated: 2022/12/20 08:27:47 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/01/07 00:15:15 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,18 +48,18 @@ static void	get_max_altitude(t_map *map)
 
 	i = 0;
 	max_z = -2147483648;
-	while (i < (*map).height)
+	while (i < map->height)
 	{
 		j = 0;
-		while (j < (*map).width)
+		while (j < map->width)
 		{
-			if ((*map).arr[i][j].z > max_z)
-				max_z = (*map).arr[i][j].z;
+			if (map->arr[i][j].z > max_z)
+				max_z = map->arr[i][j].z;
 			j++;
 		}
 		i++;
 	}
-	(*map).max_z = max_z;
+	map->max_z = max_z;
 }
 
 static void	get_min_altitude(t_map *map)
@@ -69,19 +69,19 @@ static void	get_min_altitude(t_map *map)
 	int	min_z;
 
 	i = 0;
-	min_z = (*map).max_z;
-	while (i < (*map).height)
+	min_z = map->max_z;
+	while (i < map->height)
 	{
 		j = 0;
-		while (j < (*map).width)
+		while (j < map->width)
 		{
-			if ((*map).arr[i][j].z < min_z)
-				min_z = (*map).arr[i][j].z;
+			if (map->arr[i][j].z < min_z)
+				min_z = map->arr[i][j].z;
 			j++;
 		}
 		i++;
 	}
-	(*map).min_z = min_z;
+	map->min_z = min_z;
 }
 
 static int	is_colorized(t_map *map)
@@ -90,12 +90,12 @@ static int	is_colorized(t_map *map)
 	int	j;
 
 	i = 0;
-	while (i < (*map).height)
+	while (i < map->height)
 	{
 		j = 0;
-		while (j < (*map).width)
+		while (j < map->width)
 		{
-			if ((*map).arr[i][j].color != CYAN)
+			if (map->arr[i][j].color != CYAN)
 				return (1);
 			j++;
 		}
