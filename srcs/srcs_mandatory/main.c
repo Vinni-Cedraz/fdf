e8fb5a6a38d9b027fad777ae10330fa96fab1616
Vinni-Cedraz <vcedraz-@student.42sp.org.br>
2023-01-08 17:20:17 -0300

    elem-fract on isometric state was slighty off on mandatory version

--- srcs/srcs_mandatory/main.c
+++ srcs/srcs_mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 23:00:00 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/01/07 00:00:34 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/01/08 17:14:29 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ int	main(int argc, char **argv)
 	(void)argc;
 	d = ft_calloc(sizeof(t_data), 1);
 	d->img = malloc(sizeof(t_img));
-	if (!parse_map(argv[1], d))
+	d->argv = argv;
+	if (!parse_map(d->argv[1], d))
 		return (0);
 	data_initializer(d);
 	mlx_loop_hook(d->mlx_ptr, draw, d);
