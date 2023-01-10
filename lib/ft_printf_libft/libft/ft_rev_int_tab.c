b16a0d49c44ae8cc4cf4eea1f5897f721d2399ca
Vinni-Cedraz <vcedraz-@student.42sp.org.br>
2023-01-10 10:29:46 -0300

    libft update: more readable atoi

--- lib/ft_printf_libft/libft/ft_rev_int_tab.c
+++ lib/ft_printf_libft/libft/ft_rev_int_tab.c
@@ -6,47 +6,52 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 16:28:03 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/01/07 17:27:47 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/01/10 09:45:56 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 typedef unsigned char	t_uc;
-static inline void		*aux_ft_memcpy(void *dst, const void *src, size_t n);
-static inline void		aux_ft_swap(void *a, void *b, size_t size);
+static inline void		*aux_memcpy(void *dst, const void *src, size_t n);
+static inline void		aux_swap(void *a, void *b, size_t size);
 
 void	ft_rev_int_tab(size_t *tab, size_t size)
 {
-	t_ools	tool;
+	t_counters	count;
 
-	tool.from_start = 0;
-	tool.from_end = size - 1;
-	while (tool.from_start < (size / 2))
+	count.from_start = 0;
+	count.from_end = size - 1;
+	while (count.from_start < (size / 2))
 	{
-		aux_ft_swap(&tab[tool.from_start], &tab[tool.from_end], sizeof(size_t));
-		tool.from_start++;
-		tool.from_end--;
+		aux_swap(&tab[count.from_start], &tab[count.from_end], sizeof(size_t));
+		count.from_start++;
+		count.from_end--;
 	}
 }
 
-static inline void	aux_ft_swap(void *a, void *b, size_t size)
+static inline void	aux_swap(void *a, void *b, size_t size)
 {
 	void	*tmp;
 
 	tmp = malloc(size);
-	aux_ft_memcpy(tmp, a, size);
-	aux_ft_memcpy(a, b, size);
-	aux_ft_memcpy(b, tmp, size);
+	aux_memcpy(tmp, a, size);
+	aux_memcpy(a, b, size);
+	aux_memcpy(b, tmp, size);
 	free(tmp);
 }
 
-static inline void	*aux_ft_memcpy(void *dst, const void *src, size_t n)
+static inline void	*aux_memcpy(void *dst, const void *src, size_t n)
 {
+	t_uc	*dest;
+	t_uc	*source;
 	size_t	i;
 
 	i = -1;
+	dest = (t_uc *)dst;
+	source = (t_uc *)src;
 	while (++i < n)
-		*(((t_uc *)dst) + i) = *(((t_uc *)src) + i);
-	return (dst);
+		*(dest + i) = *(source + i);
+	return (dest);
 }
