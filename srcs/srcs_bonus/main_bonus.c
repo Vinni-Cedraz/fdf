75d0173f5825178570e331925478ac8f05cd9b2f
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-01-11 16:41:56 -0300

    main_bonuus calls now only the wip functions
    It also mallocs and frees d->map directly
    - done because it takes away this burden from parse_map_bonus
    Changes to be committed:
      modified:   srcs/srcs_bonus/main_bonus.c

--- srcs/srcs_bonus/main_bonus.c
+++ srcs/srcs_bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 20:08:03 by vcedraz-          #+#    #+#             */
-/*   Updated: 2022/12/31 19:06:42 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/01/11 14:57:25 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@ int	main(int argc, char **argv)
 
 	(void)argc;
 	d = ft_calloc(sizeof(t_data), 1);
-	d->img = malloc(sizeof(t_img));
+	d->map = malloc(sizeof(t_map));
+	// d->img = malloc(sizeof(t_img));
 	if (!parse_map_bonus(argv[1], d))
 		return (0);
-	data_initializer_bonus(d);
-	mlx_loop_hook(d->mlx->mlx_ptr, draw_bonus, d);
-	mlx_hook(d->mlx->win_ptr, 02, 1L << 0, deal_keys_bonus, d);
-	mlx_hook(d->mlx->win_ptr, 17, 0, close_win_bonus, d);
-	mlx_loop(d->mlx->mlx_ptr);
-	free(d->img);
+	// data_initializer_bonus(d);
+	// mlx_loop_hook(d->mlx->mlx_ptr, draw_bonus, d);
+	// mlx_hook(d->mlx->win_ptr, 02, 1L << 0, deal_keys_bonus, d);
+	// mlx_hook(d->mlx->win_ptr, 17, 0, close_win_bonus, d);
+	// mlx_loop(d->mlx->mlx_ptr);
+	// free(d->img);
+	free(d->map);
 	free(d);
 }
