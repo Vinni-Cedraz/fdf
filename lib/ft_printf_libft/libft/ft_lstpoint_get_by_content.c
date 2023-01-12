8c86f606c6cc6071d39cbb2773027e8bab1532a5
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-01-12 16:28:22 -0300

    Just norminette

--- lib/ft_printf_libft/libft/ft_lstpoint_get_by_content.c
+++ lib/ft_printf_libft/libft/ft_lstpoint_get_by_content.c
@@ -6,15 +6,15 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 15:41:30 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/01/12 15:43:58 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/01/12 16:20:52 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_bonus.h"
 
-typedef double d;
+typedef t_node_with_a_point	t_p_node;
 
-int	ft_lstpoint_getby_content(t_node_with_a_point *lst, d x, d y, d z)
+int	ft_lstpoint_getby_content(t_p_node *lst, double x, double y, t z)
 {
 	int	i;
 
