436c614d1d6252ffb784fa255fd9da4c419d19a2
Vinni-Cedraz <vcedraz-@student.42sp.org.br>
2023-01-12 19:00:00 -0300

    Using fscanf to read and assign z values efficiently.  Changes to be committed: 	modified:   srcs/srcs_bonus/parse_map_bonus.c

--- srcs/srcs_bonus/parse_map_bonus.c
+++ srcs/srcs_bonus/parse_map_bonus.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 18:58:02 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/01/12 18:30:50 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/01/12 19:02:29 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 #include "fdf_includes_bonus.h"
 #include "t_point_bonus.h"
 
-static void	get_map_dimensions(t_data *d);
-static void	assign_point_coordinates_xy(t_data *d);
+static inline void	get_map_dimensions(t_data *d);
+static inline void	assign_point_coordinates_xy(t_data *d);
+static inline void	assign_coordinate_z(t_data *d);
 
 int	parse_map_bonus(t_data *d)
 {
@@ -33,21 +34,37 @@ int	parse_map_bonus(t_data *d)
 	while (d->tool.map_size-- > 1)
 		ft_lstpoint_front(&d->map->pts, ft_lstpoint_new());
 	assign_point_coordinates_xy(d);
+	assign_coordinate_z(d);
 	return (1);
 }
 
-static void assign_point_coordinates_xy(t_data *d)
+static void	assign_point_coordinates_xy(t_data *d)
 {
-    t_node_with_a_point *tmp = d->map->pts;
-    int counter = 0;
+	t_node_with_a_point	*tmp;
+	int					counter;
 
-    while (tmp)
-    {
-        tmp->point.x = counter % d->map->width;
-        tmp->point.y = (int)((double)counter / d->map->width);
-        tmp = tmp->next;
-        counter++;
-    }
+	tmp = d->map->pts;
+	counter = 0;
+	while (tmp)
+	{
+		tmp->point.x = counter % d->map->width;
+		tmp->point.y = (int)((double)counter / d->map->width);
+		tmp = tmp->next;
+		counter++;
+	}
+}
+
+static inline void	assign_coordinate_z(t_data *d)
+{
+	t_node_with_a_point	*tmp;
+
+	tmp = d->map->pts;
+	while (tmp)
+	{
+		fscanf(d->tool.fp, "%lf", &tmp->point.z);
+		tmp = tmp->next;
+	}
+	fclose(d->tool.fp);
 }
 
 static void	get_map_dimensions(t_data *d)
@@ -66,5 +83,5 @@ static void	get_map_dimensions(t_data *d)
 	free(first_line);
 	d->map->width = split_to_count_width->words;
 	ft_free_t_split(split_to_count_width);
-	fclose(d->tool.fp);
+	rewind(d->tool.fp);
 }
