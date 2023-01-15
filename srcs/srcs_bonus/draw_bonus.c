ce56872cbbf43d8ac2d968653d2940f053be4690
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-01-15 12:38:37 -0300

    paintitblack avoids nested loop with arithmetics

--- srcs/srcs_bonus/draw_bonus.c
+++ srcs/srcs_bonus/draw_bonus.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 23:47:42 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/01/15 12:33:48 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/01/15 12:38:19 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,19 +30,19 @@ int	draw_bonus(t_data *d)
 		render_map_bonus(d, 0, 0, 1);
 	ft_free_2d_arr((void **)d->map->arr, d->map->height);
 	draw_menu_background(d->img);
-	mlx_put_image_to_window(d->mlx->mlx_ptr, d->mlx->win_ptr, d->img->mlx_img, \
-		0, 0);
+	mlx_put_image_to_window(d->mlx->mlx_ptr, d->mlx->win_ptr, d->img->mlx_img,
+			0, 0);
 	draw_menu(d);
 	return (0);
 }
 
 static void	draw_menu_background(t_img *img)
 {
-	int counter;
-	int menu_size;
-	int i;
-	int j;
-	
+	int	counter;
+	int	menu_size;
+	int	i;
+	int	j;
+
 	counter = 0;
 	menu_size = MENU_WIDTH * img->height;
 	while (counter < menu_size)
@@ -56,19 +56,19 @@ static void	draw_menu_background(t_img *img)
 
 static void	paint_it_black(t_data *data)
 {
+	int	counter;
+	int	img_size;
 	int	i;
 	int	j;
 
-	i = 0;
-	while (i < data->img->height)
+	counter = 0;
+	img_size = data->img->width * data->img->height;
+	while (counter < img_size)
 	{
-		j = 0;
-		while (j < data->img->width)
-		{
-			put_pixel_img_bonus(data->img, j, i, BLACK);
-			j++;
-		}
-		i++;
+		i = counter / data->img->height;
+		j = counter % data->img->height;
+		put_pixel_img_bonus(data->img, i, j, BLACK);
+		counter++;
 	}
 }
 
