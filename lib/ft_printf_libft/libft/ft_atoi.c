a897f2f546c2e60a575402cd630e531046c1b8f3
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-01-17 14:17:40 -0300

    Made ft_atoi more readable and explicit with aux
    Aux functions perform the same simple instructions, but they have
    explicit names, while the instructions themselves are not
    self-explanatory
    - cleaner code, and since the aux functions are simple and inlined,
      there's no performance overhead at all

--- lib/ft_printf_libft/libft/ft_atoi.c
+++ lib/ft_printf_libft/libft/ft_atoi.c
@@ -6,30 +6,33 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 13:49:17 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/01/10 10:29:12 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/01/17 14:16:56 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static inline short	check_if_whitespace(char c);
+static inline short	is_whitespace(char c);
+static inline int	isdigit(int c);
+static inline int	issign(int c);
 
 int	ft_atoi(const char *asci)
 {
 	short	sign;
-	short	i;
+	int		i;
 	int		to_int;
 
 	i = 0;
 	sign = 1;
 	to_int = 0;
-	while (check_if_whitespace(asci[i]))
+	while (is_whitespace(asci[i]))
 		i++;
-	if (asci[i] == '-')
+	if (issign(asci[i]))
+	{
 		sign = -1;
-	if (asci[i] == '-' || asci[i] == '+')
 		i++;
-	while (asci[i] >= '0' && asci[i] <= '9')
+	}
+	while (isdigit(asci[i]))
 	{
 		to_int = to_int * 10 + (asci[i] - '0');
 		i++;
@@ -38,7 +41,7 @@ int	ft_atoi(const char *asci)
 	return (to_int);
 }
 
-static inline short	check_if_whitespace(char c)
+static inline short	is_whitespace(char c)
 {
 	if (c == SPACE)
 		return (1);
@@ -51,6 +54,21 @@ static inline short	check_if_whitespace(char c)
 	else if (c == VERTICAL_TAB)
 		return (1);
 	else if (c == FORMFEED)
+		return (1);
+	else
+		return (0);
+}
+
+static inline int	isdigit(int c)
+{
+	return ('0' <= c && c <= '9');
+}
+
+static inline int	issign(int c)
+{
+	if (c == '-')
+		return (-1);
+	else if (c == '+')
 		return (1);
 	else
 		return (0);
