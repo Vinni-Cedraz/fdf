56e14612e96e23fe43f2b1c6f7184b9dc16b51f3
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-01-17 00:24:18 -0300

    ft_lstpoint_new t_point initialization fixed
    It was missing the initialization of the t_raw field inside t_snapshot

--- lib/ft_printf_libft/libft/ft_lstpoint_new.c
+++ lib/ft_printf_libft/libft/ft_lstpoint_new.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 12:59:57 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/01/16 17:46:50 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/01/17 00:10:10 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_node_with_a_point	*ft_lstpoint_new(void)
 	t_node_with_a_point	*node;
 
 	node = (t_node_with_a_point *)malloc(sizeof(t_node_with_a_point));
-	node->point = (t_point){0, 0, 0, 0, {0, 0, 0}, {0, 0, 0}, {0,0,0}};
+	node->point = (t_point){0, 0, 0, 0, {0, 0, 0}, {0, 0, 0, {0, 0, 0}}};
 	node->next = NULL;
 	return (node);
 }
