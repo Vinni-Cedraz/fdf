04681316766758765a61d56dbcbcbba26a21f151
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-01-17 12:34:37 -0300

    Included tolerance value for comparing z to max_z
    Because in some maps point.z was never being evaluated as equal to max_z
    - which made these maps lack the MAGENTA color for the points of max
      altitude
    - 0.1 tolerance fixed this.

--- srcs/srcs_bonus/colorize_points_bonus.c
+++ srcs/srcs_bonus/colorize_points_bonus.c
@@ -6,48 +6,50 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 03:22:47 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/01/15 22:42:52 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/01/17 12:48:36 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf_includes_bonus.h"
-#include "t_point_bonus.h"
 
-// static t_short	is_colorized(t_data *d);
+static t_short	is_colorized(t_data *d);
 
 void	colorize_points_bonus(t_data *d)
 {
 	t_node_with_a_point	*tmp;
+	double 				tolerance;
 
-	// if (is_colorized(d))
-	// 	return ;
+	
+	if (is_colorized(d))
+		return ;
+	tolerance = 0.1;
 	tmp = d->map->pts;
 	while (tmp)
 	{
-		if (tmp->point.z < 0)
-			tmp->point.color = RED;
-		if (tmp->point.z == d->map->min_z)
+		if (tmp->point.z == 0)
+			tmp->point.color = WHITE;
+		else if (tmp->point.z == d->map->min_z)
 			tmp->point.color = BLUE;
-		else if (tmp->point.z == d->map->max_z)
-			d->map->pts->point.color = MAGENTA;
+		else if (tmp->point.z < 0)
+			tmp->point.color = RED;
+		else if (tmp->point.z > d->map->max_z - tolerance)
+			tmp->point.color = MAGENTA;
 		else if (tmp->point.z > 0)
 			tmp->point.color = CYAN;
-		else if (tmp->point.z == 0)
-			tmp->point.color = LIGHT_GRAY;
 		tmp = tmp->next;
 	}
 }
-//
-// static t_short	is_colorized(t_data *d)
-// {
-// 	t_node_with_a_point	*tmp;
-//
-// 	tmp = d->map->pts;
-// 	while (tmp)
-// 	{
-// 		if (tmp->point.color != CYAN)
-// 			return (1);
-// 		tmp = tmp->next;
-// 	}
-// 	return (0);
-// }
+
+static t_short	is_colorized(t_data *d)
+{
+	t_node_with_a_point	*tmp;
+
+	tmp = d->map->pts;
+	while (tmp)
+	{
+		if (tmp->point.color != CYAN)
+			return (1);
+		tmp = tmp->next;
+	}
+	return (0);
+}
