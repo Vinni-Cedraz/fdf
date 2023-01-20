d07e74d1d4e0f070f136ff0ebd6efde55cd0d583
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-01-20 14:37:20 -0300

    Clean, readable and beautiful multiply_two_matrices_bonus
    Treating each row in matrix a as a vector to be transformed individually
      by matrix b;
    - because that's what is happening mathematically when you multiply two
      matrices and it even helps making the steps clear and the code clean.

--- srcs/srcs_bonus/multiply_two_matrices_bonus.c
+++ srcs/srcs_bonus/multiply_two_matrices_bonus.c
@@ -6,61 +6,40 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 19:16:20 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/01/19 23:46:48 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/01/20 14:23:05 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf_includes_bonus.h"
 
-static t_row		get_first_row(t_matrix a, t_matrix b);
-static t_row		get_second_row(t_matrix a, t_matrix b);
-static t_row		get_third_row(t_matrix a, t_matrix b);
+static t_row		transform_vector(t_row vec, t_matrix b);
 
+// treats each row of matrix "a" as a vector to be transformed by matrix "b"
 t_matrix	multiply_two_matrices_bonus(t_matrix a, t_matrix b)
 {
-	t_matrix	result;
+	t_matrix	final_matrix;
 
-	result.row_1 = get_first_row(a, b);
-	result.row_2 = get_second_row(a, b);
-	result.row_3 = get_third_row(a, b);
-	return (result);
+	final_matrix.row_1 = transform_vector(a.row_1, b);
+	final_matrix.row_2 = transform_vector(a.row_2, b);
+	final_matrix.row_3 = transform_vector(a.row_3, b);
+	return (final_matrix);
 }
 
-static inline t_row	get_first_row(t_matrix a, t_matrix b)
+//multiplies a's first row by all three columns of b
+static inline t_row	transform_vector(t_row vec, t_matrix m)
 {
-	t_row	first_row;
+	t_row		final_vector;
+	t_emporary	v;
+	t_emporary	m_col1;
+	t_emporary	m_col2;
+	t_emporary	m_col3;
 
-	first_row.col_1 = a.row_1.col_1 * b.row_1.col_1 + a.row_1.col_2
-		* b.row_2.col_1 + a.row_1.col_3 * b.row_3.col_1;
-	first_row.col_2 = a.row_1.col_1 * b.row_1.col_2 + a.row_1.col_2
-		* b.row_2.col_2 + a.row_1.col_3 * b.row_3.col_2;
-	first_row.col_3 = a.row_1.col_1 * b.row_1.col_3 + a.row_1.col_2
-		* b.row_2.col_3 + a.row_1.col_3 * b.row_3.col_3;
-	return (first_row);
-}
-
-static inline t_row	get_second_row(t_matrix a, t_matrix b)
-{
-	t_row	second_row;
-
-	second_row.col_1 = a.row_2.col_1 * b.row_1.col_1 + a.row_2.col_2
-		* b.row_2.col_1 + a.row_2.col_3 * b.row_3.col_1;
-	second_row.col_2 = a.row_2.col_1 * b.row_1.col_2 + a.row_2.col_2
-		* b.row_2.col_2 + a.row_2.col_3 * b.row_3.col_2;
-	second_row.col_3 = a.row_2.col_1 * b.row_1.col_3 + a.row_2.col_2
-		* b.row_2.col_3 + a.row_2.col_3 * b.row_3.col_3;
-	return (second_row);
-}
-
-static inline t_row	get_third_row(t_matrix a, t_matrix b)
-{
-	t_row	third_row;
-
-	third_row.col_1 = a.row_3.col_1 * b.row_1.col_1 + a.row_3.col_2
-		* b.row_2.col_1 + a.row_3.col_3 * b.row_3.col_1;
-	third_row.col_2 = a.row_3.col_1 * b.row_1.col_2 + a.row_3.col_2
-		* b.row_2.col_2 + a.row_3.col_3 * b.row_3.col_2;
-	third_row.col_3 = a.row_3.col_1 * b.row_1.col_3 + a.row_3.col_2
-		* b.row_2.col_3 + a.row_3.col_3 * b.row_3.col_3;
-	return (third_row);
+	v = (t_emporary){vec.col_1, vec.col_2, vec.col_3};
+	m_col1 = (t_emporary){m.row_1.col_1, m.row_2.col_1, m.row_3.col_1};
+	m_col2 = (t_emporary){m.row_1.col_2, m.row_2.col_2, m.row_3.col_2};
+	m_col3 = (t_emporary){m.row_1.col_3, m.row_2.col_3, m.row_3.col_3};
+	final_vector.col_1 = (v.x * m_col1.x) + (v.y * m_col1.y) + (v.z * m_col1.z);
+	final_vector.col_2 = (v.x * m_col2.x) + (v.y * m_col2.y) + (v.z * m_col2.z);
+	final_vector.col_3 = (v.x * m_col3.x) + (v.y * m_col3.y) + (v.z * m_col3.z);
+	return (final_vector);
 }
